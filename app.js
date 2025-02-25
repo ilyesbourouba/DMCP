e2e2ba91da71b9eccf82ebd7928e8e6218376cbb
dev_NTIC <dep.ntic@gmail.com>
2025-02-25 15:22:53 +0100

    add login system with passport

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const express = require("express");
 const path = require("path");
+// for the login system
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+require('./config/passport')(passport);
+// hashing algorithm
+const bcrypt = require('bcryptjs');
 
+// for the Requests
 const cors = require('cors');
 
 const app = express();
 require('dotenv').config();
 
 app.use(cors());
-
 app.use(cors({
-    origin: ['https://streamsystem.com', 'https://streamapp.streamsystem.com/', 'http://127.0.0.1:3001/'], // Replace with your frontend's domain
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders:'*',
+    origin: ['*'], // Replace with the frontend's domain
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], 
+    allowedHeaders:'*', 
   }));
-  
+
+// setup the Engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, '/views'));
+
+// setup sessions
+app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 //setup static folder
 app.use(express.static(path.join(__dirname, "public")));
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
 
 //setup routes
 const loginRoutes = require("./routes/login");
--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const passport = require('passport');
+
 const userController = require("../controller/user");
 
 const router = express.Router();
 const { loginPage } = userController;
 
+// Login Page
 router.get("/", (req, res, next) => {
         next();
 }, loginPage);
 
-router.post("/login",loginPage);
+// Login
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/'
+}));
+
+// Logout
+router.get('/logout', (req, res, next) => {
+    req.logout(err => {
+        if (err) return next(err);
+        res.redirect('/');
+    });
+});
+
+// Middleware to check authentication
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/login');
+}
 
 module.exports = router;
