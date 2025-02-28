2fcb76bbe0722614914c90bcaf11dc26735c2e2a
dev-ntic <dev.ntic@bomarecompany.com>
2025-02-28 17:14:33 +0100

    add product page

--- controller/dashboard.js
+++
@@ -1,7 +0,0 @@
-exports.dashBoardPage = (req, res, next) => {
-    return res.render('dashboard', {
-        user: req.user,
-        sectionName: "Dashboard",
-        pageName: "bashboard"
-    });
-}
--- routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { isAuthenticated } = require("../config/authcheck");
-const { dashBoardPage } = require("../controller/dashboard");
-// dashboard Page
-router.get("/", isAuthenticated, dashBoardPage);
-
-module.exports = router;
