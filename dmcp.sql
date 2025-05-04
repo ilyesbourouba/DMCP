2b436b2f3808a4e6f59a7beb460e2d51bd0e082a
dev-ntic <dev.ntic@bomarecompany.com>
2025-05-04 12:27:06 +0100

    db

--- dmcp.sql
+++ dmcp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 24 avr. 2025 à 00:58
+-- Généré le : dim. 04 mai 2025 à 12:42
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 8.1.12
 
@@ -29,8 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
-  `name` varchar(1000) NOT NULL,
-  `description` text DEFAULT NULL,
+  `name_en` varchar(1000) NOT NULL,
+  `name_fr` varchar(1000) NOT NULL,
+  `name_ar` varchar(1000) NOT NULL,
   `image` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -38,11 +39,13 @@ CREATE TABLE `category` (
 -- Déchargement des données de la table `category`
 --
 
-INSERT INTO `category` (`id`, `name`, `description`, `image`) VALUES
-(3, 'llyes', 'Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development to fill empty spaces in a layout that does not yet have content.', NULL),
-(4, 'dino', 'Lorem ipsum is a dummy .', NULL),
-(8, 'arcane', 'arcane arcane', NULL),
-(11, 'Deadpool', 'Deadpool & Wolverine', NULL);
+INSERT INTO `category` (`id`, `name_en`, `name_fr`, `name_ar`, `image`) VALUES
+(3, 'llyes_en', 'llyes_FR', 'لييس', 'https://i.pinimg.com/564x/c1/41/67/c14167b5aa47dbc8b4e61faf54460173.jpg'),
+(4, 'dino_EN', 'dino_FR', 'ديناصور', 'https://i.pinimg.com/564x/c1/41/67/c14167b5aa47dbc8b4e61faf54460173.jpg'),
+(8, 'srir_EN', 'srir', 'غامض', 'https://i.pinimg.com/564x/c1/41/67/c14167b5aa47dbc8b4e61faf54460173.jpg'),
+(11, 'Deadpool_EN', 'Deadpool_FR', 'تجمع القتلى', 'https://i.pinimg.com/564x/c1/41/67/c14167b5aa47dbc8b4e61faf54460173.jpg'),
+(13, 'NACER DJITLI', 'caty', 'DJITLI', 'https://i.pinimg.com/564x/c1/41/67/c14167b5aa47dbc8b4e61faf54460173.jpg'),
+(18, 'test img', 'test img', 'test img', 'http://localhost:3000/uploads/1746317558481_test_img.png');
 
 -- --------------------------------------------------------
 
@@ -55,17 +58,18 @@ CREATE TABLE `client` (
   `name` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `adr` text NOT NULL,
   `password` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL
+  `token` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Déchargement des données de la table `client`
 --
 
-INSERT INTO `client` (`id`, `name`, `phone`, `email`, `password`, `token`) VALUES
-(1, 'ilyes', '0551316015', 'ilyes@gmail.com', '$2a$12$jFthR95e3SulaVDIOTY4dumqNJM.gUP1HoyZR/rIbimsXGDKRmvoG', '$2a$12$jFthR95e3SulaVDIOTY4dumqNJM.gUP1HoyZR/rIbimsXGDKRmvoG'),
-(3, 'maroua', '0551316018', 'maroua@gmail.com', '$2a$10$i9GUBUqbvh7z63JXgC9go.KVKf6uHJamYD1Y12xA.vQ6QeJmJGnnW', '');
+INSERT INTO `client` (`id`, `name`, `phone`, `email`, `adr`, `password`, `token`) VALUES
+(1, 'ilyes', '0551316015', 'mapdoua@gmail.com', 'zeralda', '$2a$12$jFthR95e3SulaVDIOTY4dumqNJM.gUP1HoyZR/rIbimsXGDKRmvoG', '$2a$12$jFthR95e3SulaVDIOTY4dumqNJM.gUP1HoyZR/rIbimsXGDKRmvoG'),
+(15, 'maroua', '0551316018', 'maroua@gmail.com', 'zeralda', '$2a$10$Iz15ndJXYZbIl3x/J3LC2.bLotBNSxG.BH3YNM9UAHtEDnlWHkjTW', NULL);
 
 -- --------------------------------------------------------
 
@@ -78,8 +82,19 @@ CREATE TABLE `commande` (
   `client_id` int(11) DEFAULT NULL,
   `total` int(11) NOT NULL,
   `date_commande` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` enum('pending','paid','shipped','delivered','cancelled') DEFAULT 'pending'
+  `status_id` int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `client_id`, `total`, `date_commande`, `status_id`) VALUES
+(3, 1, 1250, '2025-04-29 22:49:41', 2),
+(5, 1, 530, '2025-04-29 22:50:44', 1),
+(6, 1, 530, '2025-04-29 22:57:50', 5),
+(10, 1, 170, '2025-04-30 23:39:54', 4),
+(11, 1, 290, '2025-05-01 19:15:09', 3);
 
 -- --------------------------------------------------------
 
@@ -92,6 +107,41 @@ CREATE TABLE `commande_product` (
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `commande_product`
+--
+
+INSERT INTO `commande_product` (`commande_id`, `product_id`, `quantity`) VALUES
+(3, 2, 10),
+(6, 6, 2),
+(10, 6, 1),
+(11, 2, 1),
+(11, 6, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande_status`
+--
+
+CREATE TABLE `commande_status` (
+  `id` int(11) NOT NULL,
+  `status_en` varchar(255) NOT NULL,
+  `status_fr` varchar(255) NOT NULL,
+  `status_ar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `commande_status`
+--
+
+INSERT INTO `commande_status` (`id`, `status_en`, `status_fr`, `status_ar`) VALUES
+(1, 'pending', 'en attente', 'معلق'),
+(2, 'in shipping', 'en cours de livraison', 'قيد الشحن'),
+(3, 'delivered', 'livré', 'تم التسليم'),
+(4, 'paid', 'payé', 'مدفوع'),
+(5, 'cancelled', 'annulé', 'تم الإلغاء');
 
 -- --------------------------------------------------------
 
@@ -107,14 +157,35 @@ CREATE TABLE `favoris` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `livraison`
+--
+
+CREATE TABLE `livraison` (
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `livraison`
+--
+
+INSERT INTO `livraison` (`price`) VALUES
+(99);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `pack`
 --
 
 CREATE TABLE `pack` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text DEFAULT NULL,
-  `total_price` int(11) DEFAULT NULL,
+  `name_en` varchar(255) NOT NULL,
+  `name_fr` varchar(255) NOT NULL,
+  `name_ar` varchar(255) NOT NULL,
+  `description_fr` text DEFAULT NULL,
+  `description_ar` text NOT NULL,
+  `description_en` text NOT NULL,
+  `total_price` int(11) DEFAULT 0,
   `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -122,9 +193,9 @@ CREATE TABLE `pack` (
 -- Déchargement des données de la table `pack`
 --
 
-INSERT INTO `pack` (`id`, `name`, `description`, `total_price`, `image`) VALUES
-(4, 'bourouba ilyes', '2', NULL, NULL),
-(5, 'deadpool', 'ds', NULL, NULL);
+INSERT INTO `pack` (`id`, `name_en`, `name_fr`, `name_ar`, `description_fr`, `description_ar`, `description_en`, `total_price`, `image`) VALUES
+(4, 'extension ', 'prolongation', 'امتداد', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'امتدادامتداد امتداد امتداد امتداد امتداد امتداد  امتدادامتداد امتداد امتداد امتداد امتداد امتداد  امتدادامتداد امتداد امتداد امتداد امتداد امتداد  ', 'publishing', 0, 'http://192.168.100.7:3000/uploads/1746231154093_ilyes.png'),
+(13, 'bourouba ilyes', 'ilyes', 'bourouba', 'ilyesEdit ', 'ilyesEdit ', 'ilyesEdit ', 0, 'http://192.168.100.7:3000/uploads/1746317890087_ilyes.png');
 
 -- --------------------------------------------------------
 
@@ -142,8 +213,11 @@ CREATE TABLE `pack_product` (
 --
 
 INSERT INTO `pack_product` (`pack_id`, `product_id`) VALUES
-(4, 1),
-(4, 2);
+(4, 2),
+(4, 6),
+(4, 8),
+(13, 6),
+(13, 8);
 
 -- --------------------------------------------------------
 
@@ -152,21 +226,18 @@ INSERT INTO `pack_product` (`pack_id`, `product_id`) VALUES
 --
 
 CREATE TABLE `panier` (
-  `id` int(11) NOT NULL,
-  `client_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `panier_product`
---
-
-CREATE TABLE `panier_product` (
-  `panier_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL DEFAULT 1
+  `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `panier`
+--
+
+INSERT INTO `panier` (`client_id`, `product_id`, `quantity`) VALUES
+(1, 2, 1),
+(1, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -195,8 +266,32 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name_fr`, `name_en`, `name_ar`, `desc_fr`, `desc_en`, `desc_ar`, `price`, `stock`, `category_id`, `best_selling`, `new_product`, `top_rating`) VALUES
-(1, 'masque', 'mask', 'قناع', 'Lorem ipsum est un texte factice ou de substitution couramment utilisé dans la conception graphique', 'Lorem ipsum is a dummy or placeholder text commonly used in graphic design', 'لوريم إيبسوم هو نص وهمي أو نص بديل يستخدم عادة في التصميم الجرافيكي', 120, 1000, 11, 1, 1, 1),
-(2, 'verre', 'glass', 'زجاج', 'Alors que David affronte Kevin, un groupe d\'hommes armés intervient.', 'While David confronts Kevin, a group of armed men intervene.', 'بينما يواجه ديفيد كيفن، تتدخل مجموعة من الرجال المسلحين.', 120, 45, 11, 0, 0, 0);
+(2, 'verre', 'glass', 'زجاج', 'Alors que David affronte Kevin, un groupe d\'hommes armés intervient.', 'While David confronts Kevin, a group of armed men intervene.', 'بينما يواجه ديفيد كيفن، تتدخل مجموعة من الرجال المسلحين.', 120, 45, 18, 1, 1, 0),
+(6, 'name_fr', 'name_en', 'اسم عربي', 'desc_fr', 'La transparence, le respect, l\'union, la solidarité et le triomphe sont les valeurs fondamentales qui ont guidé notre succès et continueront à nous renforcer à l\'avenir.\n* ilyes bourouba \n* meroua souici', 'desc_ar', 120, 1000, 8, 1, 1, 1),
+(8, 'Nom francais', 'Nom anglais', 'Nom arabe', 'Edit Category\r\n', 'Edit Category\r\n', 'Edit Category\r\n', 9, 9, 3, 0, 0, 1),
+(11, 'tes img', 'tes img', 'tes img', 'tes img', 'tes img', 'tes img', 2, 12, 18, 0, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product_feature`
+--
+
+CREATE TABLE `product_feature` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `feature_en` varchar(1000) NOT NULL,
+  `feature_fr` varchar(1000) NOT NULL,
+  `feature_ar` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `product_feature`
+--
+
+INSERT INTO `product_feature` (`id`, `product_id`, `feature_en`, `feature_fr`, `feature_ar`) VALUES
+(1, 1, 'call fucntion', 'fonction d\'appel', 'وظيفة الاستدعاء'),
+(2, 1, 'LED indicator', 'indicateur LED', 'مؤشر LED');
 
 -- --------------------------------------------------------
 
@@ -215,7 +310,11 @@ CREATE TABLE `product_images` (
 --
 
 INSERT INTO `product_images` (`id`, `product_id`, `image_url`) VALUES
-(3, 1, '1744463441220_mask.jpg');
+(9, 6, 'https://i.pinimg.com/564x/c1/41/67/c14167b5aa47dbc8b4e61faf54460173.jpg'),
+(12, 8, 'https://i.pinimg.com/564x/c1/41/67/c14167b5aa47dbc8b4e61faf54460173.jpg'),
+(14, 8, 'https://i.pinimg.com/564x/c1/41/67/c14167b5aa47dbc8b4e61faf54460173.jpg'),
+(15, 11, 'http://localhost:3000/uploads/1746317999558_tes_img.png'),
+(16, 2, 'http://localhost:3000/uploads/1746318052879_verre.png');
 
 -- --------------------------------------------------------
 
@@ -258,7 +357,8 @@ ALTER TABLE `client`
 --
 ALTER TABLE `commande`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `client_id` (`client_id`);
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `fk_commande_status` (`status_id`);
 
 --
 -- Index pour la table `commande_product`
@@ -266,6 +366,12 @@ ALTER TABLE `commande`
 ALTER TABLE `commande_product`
   ADD PRIMARY KEY (`commande_id`,`product_id`),
   ADD KEY `product_id` (`product_id`);
+
+--
+-- Index pour la table `commande_status`
+--
+ALTER TABLE `commande_status`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `favoris`
@@ -291,14 +397,7 @@ ALTER TABLE `pack_product`
 -- Index pour la table `panier`
 --
 ALTER TABLE `panier`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `client_id` (`client_id`);
-
---
--- Index pour la table `panier_product`
---
-ALTER TABLE `panier_product`
-  ADD PRIMARY KEY (`panier_id`,`product_id`),
+  ADD PRIMARY KEY (`client_id`,`product_id`),
   ADD KEY `product_id` (`product_id`);
 
 --
@@ -307,6 +406,12 @@ ALTER TABLE `panier_product`
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
   ADD KEY `category_id` (`category_id`);
+
+--
+-- Index pour la table `product_feature`
+--
+ALTER TABLE `product_feature`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `product_images`
@@ -330,43 +435,49 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT pour la table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT pour la table `commande`
 --
 ALTER TABLE `commande`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `commande_status`
+--
+ALTER TABLE `commande_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `pack`
 --
 ALTER TABLE `pack`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT pour la table `panier`
---
-ALTER TABLE `panier`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `product_feature`
+--
+ALTER TABLE `product_feature`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT pour la table `user`
@@ -382,7 +493,8 @@ ALTER TABLE `user`
 -- Contraintes pour la table `commande`
 --
 ALTER TABLE `commande`
-  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_commande_status` FOREIGN KEY (`status_id`) REFERENCES `commande_status` (`id`);
 
 --
 -- Contraintes pour la table `commande_product`
@@ -409,14 +521,9 @@ ALTER TABLE `pack_product`
 -- Contraintes pour la table `panier`
 --
 ALTER TABLE `panier`
-  ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `panier_product`
---
-ALTER TABLE `panier_product`
-  ADD CONSTRAINT `panier_product_ibfk_1` FOREIGN KEY (`panier_id`) REFERENCES `panier` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `panier_product_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_panier_client` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`),
+  ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`),
+  ADD CONSTRAINT `panier_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
 --
 -- Contraintes pour la table `product`
